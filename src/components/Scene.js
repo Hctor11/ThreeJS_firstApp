bb1aff47330dd4467bcc54edd9a1d7d36223d0db
hector <hectorrrivera1010@gmail.com>
2022-12-27 03:17:37 -0600

    lights

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -30,10 +30,38 @@ const Scene = () => {
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.target = new THREE.Vector3(3, 3, 3);
 
+    // Luz 
+    const AL = new THREE.AmbientLight(0xffffff, 0.4)
+    scene.add(AL)
+    // PointLight
+    const pointLight = new THREE.PointLight(0xffffff, 1);
+    pointLight.position.y = 25;
+    pointLight.position.x = 20;
+    pointLight.position.z = 25;
+
+    scene.add(pointLight)
+
+    //texture loader del cubo
+    const textureLoaderCubo = new THREE.TextureLoader();
+    const basecolor = textureLoaderCubo.load('./metal_texture/basecolor.jpg');
+    const AO = textureLoaderCubo.load('./metal_texture/ambientOcclusion.jpg');
+    const roughness = textureLoaderCubo.load('./metal_texture/roughness.jpg');
+    const normal = textureLoaderCubo.load('./metal_texture/normal.jpg');
+    const metalness = textureLoaderCubo.load('./metal_texture/metallic.jpg');
+    const heightx = textureLoaderCubo.load('./metal_texture/height.png');
+
     //creando un cubo
     const cube = new THREE.Mesh(
-      new THREE.BoxBufferGeometry(10, 10, 10),
-      new THREE.MeshBasicMaterial()
+      new THREE.BoxBufferGeometry(10, 10, 10, 250, 250, 250),
+      new THREE.MeshStandardMaterial({
+        map: basecolor,
+        aoMap: AO,
+        roughnessMap: roughness,
+        normalMap: normal,
+        metalnessMap: metalness,
+        displacementMap: heightx,
+        displacementScale: 0.5
+      })
     );
     scene.add(cube);
 
@@ -45,10 +73,15 @@ const Scene = () => {
     torus.position.x = -20;
     scene.add(torus);
 
+    // texture loader para torusKnot
+    const textureLoader = new THREE.TextureLoader();
+    const matcap = textureLoader.load('./textures/texture.png');
     // agregando una torusKnot
     const torusKnot = new THREE.Mesh(
       new THREE.TorusKnotGeometry(10, 3, 100, 16),
-      new THREE.MeshNormalMaterial({ flatShading: true })
+      new THREE.MeshMatcapMaterial({ 
+        matcap: matcap
+      })
     );
     scene.add(torusKnot);
     torusKnot.position.x = 20;
